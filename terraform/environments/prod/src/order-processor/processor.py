1e002cc30dc247e8f3cd8b773f346da297505eec
case-k-git <casekblog@gmail.com>
2025-03-12 11:22:02 +0900

    update

---
+++ terraform/environments/prod/src/order-processor/processor.py
@@ -0,0 +1,14 @@
+import json
+from typing import Dict, Any
+
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
+    """
+    シンプルなエコーバック関数
+    """
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Hello from Order Processor',
+            'event': event
+        })
+    } 
