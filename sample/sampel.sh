57e7d469be7e66b9dd3acc2f27191268e3a7a90c
case-k-git <casekblog@gmail.com>
2025-03-12 15:18:11 +0900

    updatae

--- sample/sampel.sh
+++ sample/sampel.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-# 注意: set -e を使うとエラー時に自動終了してしまうので使わない
-
 for var in DBT_RUN DBT_TEST DBT_FRESHNESS DBT_SNAPSHOT DBT_RETRY; do
   value="${!var:-}"
   if [ -n "$value" ]; then
     echo "command: $value"
     if ! $value; then
       echo "Error: command '$value' failed" >&2
-      # DBT_RUN, DBT_RETRY, DBT_SNAPSHOT の場合はスクリプトを終了しない
       case "$var" in
         DBT_RUN|DBT_RETRY|DBT_SNAPSHOT)
           echo "Skipping $var despite error"
